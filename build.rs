ce4e3b7c3563188a39af75e8a95dc8d695ecf3bf
Astavie <astavie@pm.me>
2024-04-28 16:25:49 +0200

    point translate

--- build.rs
+++ build.rs
@@ -11,12 +11,15 @@ fn main() {
         "epga1d:1,1;Scalar:1;ComplexNumber:1,e01",
         "ppga1d:0,1;Scalar:1;DualNumber:1,e01",
         "hpga1d:-1,1;Scalar:1;SplitComplexNumber:1,e01",
-        "epga2d:1,1,1;Scalar:1;PseudoScalar:e012;Rotor:1,e12;Origin:e12;Point:e12,e01,-e02;Direction:e01,-e02;Line:e0,e2,e1;Translator:1,e01,-e02;Motor:1,e12,e01,-e02;MotorDual:e012,e0,e2,e1",
-        "ppga2d:0,1,1;Scalar:1;PseudoScalar:e012;Rotor:1,e12;Origin:e12;Point:e12,e01,-e02;Direction:e01,-e02;Line:e0,e2,e1;Translator:1,e01,-e02;Motor:1,e12,e01,-e02;MotorDual:e012,e0,e2,e1",
-        "hpga2d:-1,1,1;Scalar:1;PseudoScalar:e012;Rotor:1,e12;Origin:e12;Point:e12,e01,-e02;Direction:e01,-e02;Line:e0,e2,e1;Translator:1,e01,-e02;Motor:1,e12,e01,-e02;MotorDual:e012,e0,e2,e1",
-        "epga3d:1,1,1,1;Scalar:1;PseudoScalar:e0123;Rotor:1,e23,-e13,e12;Origin:e123;Point:e123,-e023,e013,-e012;Direction:-e023,e013,-e012;IdealLine:e01,e02,e03;Branch:e23,-e13,e12;Plane:e0,e1,e2,e3;Line:e01,e02,e03|e23,-e13,e12;Translator:1,e01,e02,e03;Motor:1,e23,-e13,e12|e0123,e01,e02,e03",
-        "ppga3d:0,1,1,1;Scalar:1;PseudoScalar:e0123;Rotor:1,e23,-e13,e12;Origin:e123;Point:e123,-e023,e013,-e012;Direction:-e023,e013,-e012;IdealLine:e01,e02,e03;Branch:e23,-e13,e12;Plane:e0,e1,e2,e3;Line:e01,e02,e03|e23,-e13,e12;Translator:1,e01,e02,e03;Motor:1,e23,-e13,e12|e0123,e01,e02,e03",
-        "hpga3d:-1,1,1,1;Scalar:1;PseudoScalar:e0123;Rotor:1,e23,-e13,e12;Origin:e123;Point:e123,-e023,e013,-e012;Direction:-e023,e013,-e012;IdealLine:e01,e02,e03;Branch:e23,-e13,e12;Plane:e0,e1,e2,e3;Line:e01,e02,e03|e23,-e13,e12;Translator:1,e01,e02,e03;Motor:1,e23,-e13,e12|e0123,e01,e02,e03",
+        "epga2d:1,1,1;Scalar:1;Pseudoscalar:e012;Rotor:1,e12;Origin:e12;Point:e12,e01,-e02;Direction:e01,-e02;Line:e0,e2,e1;Translator:1,e01,-e02;Motor:1,e12,e01,-e02;MotorDual:e012,e0,e2,e1",
+        "ppga2d:0,1,1;Scalar:1;Pseudoscalar:e012;Rotor:1,e12;Origin:e12;Point:e12,e01,-e02;Direction:e01,-e02;Line:e0,e2,e1;Translator:1,e01,-e02;Motor:1,e12,e01,-e02;MotorDual:e012,e0,e2,e1",
+        "hpga2d:-1,1,1;Scalar:1;Pseudoscalar:e012;Rotor:1,e12;Origin:e12;Point:e12,e01,-e02;Direction:e01,-e02;Line:e0,e2,e1;Translator:1,e01,-e02;Motor:1,e12,e01,-e02;MotorDual:e012,e0,e2,e1",
+        "epga3d:1,1,1,1;Scalar:1;Pseudoscalar:e0123;Rotor:1,e23,-e13,e12;Origin:e123;Point:e123,-e023,e013,-e012;Direction:-e023,e013,-e012;IdealLine:e01,e02,e03;Branch:e23,-e13,e12;Plane:e0,e1,e2,e3;Line:e01,e02,e03|e23,-e13,e12;Translator:1,e01,e02,e03;Motor:1,e23,-e13,e12|e0123,e01,e02,e03",
+        "ppga3d:0,1,1,1;Scalar:1;Pseudoscalar:e0123;Rotor:1,e23,-e13,e12;Origin:e123;Point:e123,-e023,e013,-e012;Direction:-e023,e013,-e012;IdealLine:e01,e02,e03;Branch:e23,-e13,e12;Plane:e0,e1,e2,e3;Line:e01,e02,e03|e23,-e13,e12;Translator:1,e01,e02,e03;Motor:1,e23,-e13,e12|e0123,e01,e02,e03",
+        "hpga3d:-1,1,1,1;Scalar:1;Pseudoscalar:e0123;Rotor:1,e23,-e13,e12;Origin:e123;Point:e123,-e023,e013,-e012;Direction:-e023,e013,-e012;IdealLine:e01,e02,e03;Branch:e23,-e13,e12;Plane:e0,e1,e2,e3;Line:e01,e02,e03|e23,-e13,e12;Translator:1,e01,e02,e03;Motor:1,e23,-e13,e12|e0123,e01,e02,e03",
+
+        "vga3d:1,1,1;Scalar:1;Pseudoscalar:e012;Vector:e0,e1,e2;Pseudovector:e12,-e02,e01;Rotor:1,e12,-e02,e01",
+        "sta:1,-1,-1,-1;Scalar:1;Pseudoscalar:e0123;ComplexNumber:1,e0123;SpaceVector:-e01,-e02,-e03;SpacetimeSplit:-e01,-e02,-e03|1,-e23,e13,-e21;SpacetimeVector:e0,e1,e2,e3",
     ];
 
     let dir = env!("CARGO_MANIFEST_DIR");
--- src/lib.rs
+++ src/lib.rs
@@ -108,6 +108,35 @@ impl SandwichProduct<f32> for f32 {
     }
 }
 
+impl ppga2d::Point {
+    pub fn translate(self, dir: ppga2d::Direction) -> Self {
+        self + dir * self[0]
+    }
+}
+
+impl ppga3d::Point {
+    pub fn translate(self, dir: ppga3d::Direction) -> Self {
+        self + dir * self[0]
+    }
+}
+
+impl ppga2d::Origin {
+    pub fn translate(self, dir: ppga2d::Direction) -> ppga2d::Point {
+        ppga2d::Point::from_raw(self[0], dir[0] * self[0], dir[1] * self[0])
+    }
+}
+
+impl ppga3d::Origin {
+    pub fn translate(self, dir: ppga3d::Direction) -> ppga3d::Point {
+        ppga3d::Point::from_raw(
+            self[0],
+            dir[0] * self[0],
+            dir[1] * self[0],
+            dir[2] * self[0],
+        )
+    }
+}
+
 impl Magnitude for ppga2d::Direction {
     fn length_squared(self) -> f32 {
         self[0] * self[0] + self[1] * self[1]
@@ -187,8 +216,8 @@ impl ppga2d::Point {
 }
 
 impl ppga2d::Direction {
-    pub const fn new(x: f32, y: f32) -> Self {
-        Self::from_raw(y, x)
+    pub const fn new(xo: f32, yo: f32) -> Self {
+        Self::from_raw(yo, xo)
     }
 }
 
@@ -200,8 +229,7 @@ impl ppga2d::Direction {
 
 impl ppga2d::Translator {
     pub fn ln(self) -> ppga2d::Direction {
-        let result: ppga2d::Direction = self.into();
-        result * (1.0 / self[0])
+        Into::<ppga2d::Direction>::into(self) / self[0]
     }
     pub fn powf(self, exponent: f32) -> Self {
         (self.ln() * exponent).exp()
@@ -238,13 +266,12 @@ impl ppga2d::Rotor {
 
 impl ppga2d::Point {
     pub fn exp(self) -> ppga2d::Motor {
-        let det = self[0] * self[0];
-        if det <= 0.0 {
+        if self[0] == 0.0 {
             return ppga2d::Motor::from_raw(1.0, 0.0, self[1], self[2]);
         }
-        let a = det.sqrt();
-        let c = a.cos();
-        let s = a.sin() / a;
+        let a = self[0];
+        let (s, c) = a.sin_cos();
+        let s = s / a;
         let g0 = simd::Simd32x3::from(s) * self.group0();
         ppga2d::Motor::from_raw(c, g0[0], g0[1], g0[2])
     }
@@ -365,13 +392,13 @@ impl std::default::Default for ppga2d::Origin {
     }
 }
 
-impl std::default::Default for ppga3d::PseudoScalar {
+impl std::default::Default for ppga3d::Pseudoscalar {
     fn default() -> Self {
         ppga3d::I
     }
 }
 
-impl std::default::Default for ppga2d::PseudoScalar {
+impl std::default::Default for ppga2d::Pseudoscalar {
     fn default() -> Self {
         ppga2d::I
     }
@@ -380,13 +407,13 @@ impl std::default::Default for ppga2d::PseudoScalar {
 pub mod ppga3d {
     pub use super::generated::ppga3d::*;
     pub const ORIGIN: Origin = Origin::from_raw(1.0);
-    pub const I: PseudoScalar = PseudoScalar::from_raw(1.0);
+    pub const I: Pseudoscalar = Pseudoscalar::from_raw(1.0);
 }
 
 pub mod ppga2d {
     pub use super::generated::ppga2d::*;
     pub const ORIGIN: Origin = Origin::from_raw(1.0);
-    pub const I: PseudoScalar = PseudoScalar::from_raw(1.0);
+    pub const I: Pseudoscalar = Pseudoscalar::from_raw(1.0);
 
     pub const UP: Direction = Direction::new(0.0, 1.0);
     pub const DOWN: Direction = Direction::new(0.0, -1.0);
@@ -503,6 +530,8 @@ pub trait Inverse {
 
 #[cfg(test)]
 mod tests {
+    use std::f32::consts::PI;
+
     use super::*;
     use plotly::{common::Line, Plot, Scatter};
 
@@ -510,12 +539,17 @@ mod tests {
     fn it_works() {
         let mut plot = Plot::new();
 
-        const POINTS: usize = 10;
-        const ANGLE: f32 = 2.0 * std::f32::consts::PI / (POINTS as f32);
+        const POINTS: usize = 16;
+        const FRAC: f32 = 2.0 / (POINTS as f32);
 
-        let center = ppga2d::Point::new(2.0, 3.0);
+        let center = ppga2d::ORIGIN;
+        let radius = 1.0;
+
+        let rotor = (PI / 2.0 * center).exp();
+        let on_circle = center.translate(ppga2d::UP * radius);
+
         let points = (0..POINTS)
-            .map(|p| point_on_circle(center, 1.5, ANGLE * ((p + 1) as f32)))
+            .map(|p| rotor.powf(p as f32 * FRAC).sandwich(on_circle))
             .collect::<Vec<_>>();
 
         let trace = scatter(&points);
@@ -524,16 +558,28 @@ mod tests {
         plot.write_html("out.html");
     }
 
-    fn point_on_circle(center: ppga2d::Point, radius: f32, angle: f32) -> ppga2d::Point {
-        let center = center.normalized();
+    #[test]
+    fn vectorspace() {
+        use vga3d::*;
 
-        let up = ppga2d::UP * radius;
-        let on_circle = center + up;
+        let a = Vector::from_raw(1.0, 3.0, 2.0);
+        let b = Vector::from_raw(6.0, 2.0, 5.0);
 
-        let rotor = (angle / 2.0 * center).exp();
-        println!("{:?}", rotor);
+        let ab = a * b;
+
+        println!("{:?}", ab);
+        // assert!(false);
+    }
+
+    #[test]
+    fn spacetime() {
+        use sta::*;
+        const G0: SpacetimeVector = SpacetimeVector::from_raw(1.0, 0.0, 0.0, 0.0);
+
+        let v = SpacetimeVector::from_raw(1.0, 4.0, 3.0, 6.0);
+        let split = v * G0;
+        println!("{:?}", split);
         assert!(false);
-        rotor.sandwich(on_circle)
     }
 
     fn scatter(points: &[ppga2d::Point]) -> Box<Scatter<f32, f32>> {
